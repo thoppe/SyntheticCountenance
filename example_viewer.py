7135893e8edb6507fe7e2c17ea404df258de557a
Travis Hoppe <travis.hoppe@gmail.com>
2019-01-04 01:20:06 -0500

    Cleanup

--- example_viewer.py
+++ example_viewer.py
@@ -1,21 +1,9 @@
 import numpy as np
 import pixelhouse as ph
-import time
-
-
-class LiveAnimation(ph.Animation):
-
-    def show(self, delay=1):
-        while True:
-            img = self.render(0)
-            self.has_rendered[0] = False
-            img.show(delay=delay)
-
 import sys
-
 import pickle
 import tensorflow as tf
-from tqdm import tqdm
+import time
 
 config = tf.ConfigProto(allow_soft_placement=True)
 config.gpu_options.allow_growth = True
@@ -36,11 +24,25 @@ def generate_image(Gs):
     dummy = np.zeros([z.shape[0], 0])
     img = Gs.run(z, dummy)
 
+    score = D.run(img)[0].ravel()[0]
+
     # [-1,1] => [0,255]
     img = np.clip(np.rint(
         (img + 1.0) / 2.0 * 255.0), 0.0, 255.0).astype(np.uint8)
+
     img = img.transpose(0, 2, 3, 1)  # NCHW => NHWC
-    return img[0]
+    
+    return img[0], score
+
+
+class LiveAnimation(ph.Animation):
+
+    def show(self, delay=1):
+        while True:
+            img = self.render(0)
+            self.has_rendered[0] = False
+            img.show(delay=delay)
+            time.sleep(1)
 
 
 class GAN_viewer(ph.Artist):
@@ -49,9 +51,10 @@ class GAN_viewer(ph.Artist):
         pass
 
      def draw(self, cvs, t=0):
-        img = generate_image(Gs)
+        img, score = generate_image(Gs)
         cvs.img[:, :, :3] = img
-        print(img.shape)
+        cvs += ph.text(x=0, y=2, text=f"{score:0.2f}")
+
 
 A = LiveAnimation(1024, 1024)
 A += GAN_viewer()
