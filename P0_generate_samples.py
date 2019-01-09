2853fea39bed2be4404e83dfbcdbb1d13676f1d7
thoppe <travis.hoppe@gmail.com>
2019-01-09 14:41:36 -0500

    Dont force overwrites

--- P0_generate_samples.py
+++ P0_generate_samples.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from PIL import Image
-import os, json
+import os, json, glob
 
 from src.GAN_model import load_GAN_model, generate_single
 
@@ -15,13 +15,18 @@ save_dest_info = 'samples/latent_vectors'
 os.system(f'mkdir -p {save_dest_imgs}')
 os.system(f'mkdir -p {save_dest_info}')
 
-for n in tqdm(range(max_images)):
+largest_known = glob.glob(os.path.join(save_dest_imgs, '*'))
+largest_known = os.path.basename(sorted(largest_known)[-1])
+largest_idx = int(largest_known.split('.')[0])
+print("Starting generation")    
+
+for n in tqdm(range(largest_idx, max_images)):
     
     f_save = os.path.join(save_dest_imgs, f"{n:06d}.jpg")
     if os.path.exists(f_save):
         continue
     
-    img, z, ds = generate_single(Gs, D)
+    img, z, ds = generate_single(Gs, D, compute_discriminator=False)
     
     P_img = Image.fromarray(img)
     P_img.save(f_save)
