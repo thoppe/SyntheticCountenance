3fe24450b0c43f51ca03f1b21177056cde18489f
Travis Hoppe <travis.hoppe@gmail.com>
2019-01-08 21:20:12 -0500

    Refactoring pipeline

--- P0_generate_samples.py
+++ P0_generate_samples.py
@@ -4,92 +4,27 @@ from tqdm import tqdm
 from PIL import Image
 import os, json
 
-dim = 512
+from src.GAN_model import load_GAN_model, generate_single
 
-def load_model():
-    print("Loading the model")
+max_images = 10**6
 
-    import tensorflow as tf
-    import sys
-    import pickle
-
-    config = tf.ConfigProto(allow_soft_placement=False)
-    #config.gpu_options.allow_growth = True
-    config.gpu_options.per_process_gpu_memory_fraction = 0.45
-    sess = tf.InteractiveSession(config=config)
-
-
-    f_model = 'model/karras2018iclr-celebahq-1024x1024.pkl'
-    path_pg_gan_code = 'src/model/pggan/'
-
-    sys.path.append(path_pg_gan_code)
-    with open(f_model, 'rb') as FIN:
-        G, D, Gs = pickle.load(FIN)
-
-    return G, D, Gs
-
-def compute_single(generator, discriminator, compute_discriminator=True):
-
-    z = np.random.randn(dim)
-
-    zf = z[None, :]
-    dummy = np.zeros([z.shape[0], 0])
-
-    img = generator.run(zf, dummy)
-
-    if compute_discriminator:
-        ds = discriminator.run(img)[0].ravel()[0]
-    else:
-        ds = None
-
-    # [-1,1] => [0,255]
-    img = np.clip(np.rint(
-        (img + 1.0) / 2.0 * 255.0), 0.0, 255.0).astype(np.uint8)
-
-    img = img.transpose(0, 2, 3, 1)  # NCHW => NHWC
-
-    return img[0], ds, z
-
-G, D, Gs = load_model()
-save_dest_imgs = 'examples/imgs'
-save_dest_info = 'examples/info'
+G, D, Gs = load_GAN_model()
+save_dest_imgs = 'samples/images'
+save_dest_info = 'samples/latent_vectors'
 
 os.system(f'mkdir -p {save_dest_imgs}')
 os.system(f'mkdir -p {save_dest_info}')
 
-for n in tqdm(range(1000000)):
+for n in tqdm(range(max_images)):
+    
     f_save = os.path.join(save_dest_imgs, f"{n:06d}.jpg")
     if os.path.exists(f_save):
         continue
     
-    img, ds, z = compute_single(Gs, D)
-
+    img, z, ds = generate_single(Gs, D)
     
     P_img = Image.fromarray(img)
     P_img.save(f_save)
 
-    info = {
-        "z" : [float(_) for _ in z],
-        "n" : n,
-        "discriminator_score" : float(ds),         
-    }
-
-    f_save_info = os.path.join(save_dest_info, f"{n:06d}.json")
-    with open(f_save_info, 'w') as FOUT:
-        FOUT.write(json.dumps(info, indent=2))
-
-    print(f_save)
-
-
-
-'''
-for n in tqdm(range(10000)):
-    img, ds = compute_single(Gs, D)
-
-    f_save = os.path.join(save_dest, f"{-ds:0.4f}.jpg")
-
-    P_img = Image.fromarray(img)
-    P_img.save(f_save)
-
-    print(f_save)
-'''
+    f_save_info = os.path.join(save_dest_info, f"{n:06d}.npy")
+    np.save(f_save_info, z)
