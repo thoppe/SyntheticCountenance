2a276d8c7e514d7cae49b33176642d0d67935992
Travis Hoppe <travis.hoppe@gmail.com>
2019-01-09 23:42:05 -0500

    Let collect gather all data

--- C0_collect_data.py
+++ C0_collect_data.py
@@ -3,29 +3,60 @@ import os, json, glob, h5py
 import pandas as pd
 from tqdm import tqdm
 
+# Load the image attributes
+
+def collect_attributes():
+
+    F_ATTRIBUTES = sorted(glob.glob("samples/image_attributes/*.json"))
+    data = []
+    for f in tqdm(F_ATTRIBUTES):
+        with open(f) as FIN:
+            js = json.loads(FIN.read())
+            js['name'] = int(os.path.basename(f).split('.')[0])
+        data.append(js)
+    df = pd.DataFrame(data).sort_values('name').set_index('name')
+
+    label = 'image_attributes'
+    del h5[label]
+    g = h5.require_group(label)
+
+    g['image_idx'] = df.index.values
+    for key in df:
+        g[key] = df[key].values
+
+
+def collect_vectors(label):
+    print(f"Collecting {label}")
+    F_VECTORS = sorted(glob.glob(
+        f"samples/{label}/*.npy"))
+
+    X = []
+    image_idx = []
+    
+    for f in tqdm(F_VECTORS):
+        X.append(np.load(f))
+        image_idx.append(int(os.path.basename(f).split('.')[0]))
+
+    if label in h5:
+        del h5[label]
+
+    g = h5.require_group(label)
+    g['image_idx'] = image_idx
+    g[label] = X
+
+
 f_h5 = 'samples/PGAN_attributes.h5'
 if not os.path.exists(f_h5):
     with h5py.File(f_h5, 'w'):
         pass
 h5 = h5py.File(f_h5, 'r+')
+    
+if __name__ == "__main__":
+    collect_attributes()
+    collect_vectors('face_vectors')
+    collect_vectors('latent_vectors')
+    collect_vectors('keypoints')
 
-# Load the image attributes
-
-F_ATTRIBUTES = sorted(glob.glob("samples/image_attributes/*.json"))
-data = []
-for f in tqdm(F_ATTRIBUTES):
-    with open(f) as FIN:
-        js = json.loads(FIN.read())
-        js['name'] = int(os.path.basename(f).split('.')[0])
-    data.append(js)
-df = pd.DataFrame(data).sort_values('name').set_index('name')
-
-del h5['image_attributes']
-g = h5.require_group("image_attributes")
-
-g['image_idx'] = df.index.values
-for key in df:
-    g[key] = df[key].values
 
 
 
