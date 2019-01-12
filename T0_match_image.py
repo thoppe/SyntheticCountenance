f71c7a67e6096f2073ee5fcb0fb1f7e7b9017c4d
Travis Hoppe <travis.hoppe@gmail.com>
2019-01-11 23:58:52 -0500

    Moved model into function

--- T0_match_image.py
+++ T0_match_image.py
@@ -2,42 +2,50 @@ import numpy as np
 from PIL import Image
 import os, json, glob
 import tensorflow as tf
-from src.GAN_model import load_GAN_model, raw_GAN_output_to_RGB
-import h5py
+from src.GAN_model import load_GAN_model
+from src.GAN_model import GAN_output_to_RGB, RGB_to_GAN_output
 
-latent_dim = 512
-image_dim = 1024
-batch_size = 1
 
 G, D, Gs, sess = load_GAN_model(return_sess=True)
 
-#z0 = np.random.randn(latent_dim)
-f_npy = 'samples/latent_vectors/000360.npy'
-z0 = np.load(f_npy)
+def find_latent_from_image_model(generator):
+    latent_dim = 512
+    image_dim = 1024
+    batch_size = 1
 
-label_dummy = tf.zeros([batch_size,0])
-latents = tf.Variable(z0[None, :], dtype=tf.float32)
-G_out = Gs.get_output_for(latents, label_dummy, is_training=False)
+    z0 = np.random.randn(latent_dim)
+    label_dummy = tf.zeros([batch_size,0])
+    
+    latents = tf.Variable(z0[None, :], dtype=tf.float32)
+    G_out = generator.get_output_for(
+        latents, label_dummy, is_training=False)
 
-# NCHW
-target_images = tf.placeholder(
-    tf.float32, shape=(batch_size, 3, image_dim, image_dim))
+    # NCHW
+    target_images = tf.placeholder(
+        tf.float32, shape=(batch_size, 3, image_dim, image_dim))
 
-# L1 error, only train the loss
-loss = tf.reduce_sum(tf.abs(G_out - target_images))
-optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
-train_op = optimizer.minimize(loss, var_list=[latents,])
+    # L1 error, only train the loss
+    loss = tf.reduce_sum(tf.abs(G_out - target_images))
+    loss /= image_dim**2
+    
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+    train_op = optimizer.minimize(loss, var_list=[latents,])
+
+    return latents, target_images, loss, optimizer, train_op
+
 
 # Load in the target image and bring it proper format
-f_image = 'src/000260.jpg'
-
 # Open the image and scale it to [-1, 1] and CHW
+f_image = 'src/000260.jpg'
 img = Image.open(f_image)
-img = np.array(img).transpose(2,0,1).astype(float)
-img = 2*(img/255.0) - 1
-grid = np.tile(img, (batch_size, 1, 1, 1))
+grid = RGB_to_GAN_output(img)
+
+#f_npy = 'samples/latent_vectors/000360.npy'
+#z0 = np.load(f_npy)
 
 print ("Starting training")
+
+latents, target_images, loss, optimizer, train_op = find_latent_from_image_model(Gs)
 
 init_new_vars_op = tf.initializers.variables([latents,])
 sess.run(init_new_vars_op)
@@ -46,7 +54,7 @@ init_new_vars_op = tf.initializers.variables(optimizer.variables())
 sess.run(init_new_vars_op)
 
 lx = sess.run(loss, feed_dict={target_images:grid})
-print(f"Starting loss: {lx / image_dim**2}")
+print(f"Starting loss: {lx}")
 
 save_dest = 'training_demo'
 
@@ -55,11 +63,9 @@ for i in range(200000):
     if i%10==0:
 
         z_current = sess.run(latents, feed_dict={target_images:grid})
-
-        print("Z_ERROR", np.linalg.norm(z0-z_current))
         img = Gs.run(z_current, np.zeros([512,0]))
 
-        img = raw_GAN_output_to_RGB(img)[0]
+        img = GAN_output_to_RGB(img)[0]
         P_img = Image.fromarray(img)
         f_save = f'{save_dest}/{i:05d}.jpg'
 
@@ -67,13 +73,9 @@ for i in range(200000):
             os.system(f'rm -vf {save_dest}/*')
         
         P_img.save(f_save)
-        #print(f_save)
-        #exit()
         
     outputs = [loss, train_op]
     lx,_ = sess.run(outputs, feed_dict={target_images:grid})
-    lx /= image_dim**2
-
     
     print(i, lx)
     
