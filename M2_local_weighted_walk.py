9a712d042f8a82366441e3793cbde3162ac50263
Travis Hoppe <travis.hoppe@gmail.com>
2019-01-14 22:45:26 -0500

    Update local movie walk

--- M2_local_weighted_walk.py
+++ M2_local_weighted_walk.py
@@ -1,87 +1,89 @@
-import pixelhouse as ph
-import cv2
-
 import numpy as np
-import pandas as pd
 from tqdm import tqdm
 from PIL import Image
-import os, json
+import os, json, glob
 import scipy.stats
-from src.GAN_model import load_GAN_model, generate_single
+from src.GAN_model import load_GAN_model, generate_single, logger
 
 
 def interpolate(Z, frames_per=30):
     n = len(Z)
     T = np.linspace(0, n - 1, n * frames_per)
 
-    sigma = 0.50
-    G = [scipy.stats.norm(loc=k, scale=sigma) for k in range(n)]
+    sigma = np.random.uniform(low=0.5, high=0.75, size=[n,])
+    G = [scipy.stats.norm(loc=k, scale=s) for k,s in zip(range(n), sigma)]
 
-    motion = ph.motion.easeInOutQuad(0, 1)
-    C = ph.Canvas(1024, 1024)
-
-    for frame_idx, t in enumerate(T):
+    for frame_idx, t in tqdm(enumerate(T), total=len(T)):
         weights = np.array([g.pdf(t) for g in G])
 
         # Need to wrap around weights for perfect loop
         weights += np.array([g.pdf(t + T.max()) for g in G])
         weights += np.array([g.pdf(t - T.max()) for g in G])
 
-        # Normal
+        # Set the strength of the weights to be unity
         weights /= weights.sum()
 
         zx = (Z.T * weights).sum(axis=1)
         zx /= np.sqrt((weights ** 2).sum())
 
         img, *_ = generate_single(Gs, D, z=zx, compute_discriminator=False)
-        C.img[:, :, :3] = img
 
         f_save = os.path.join(save_dest, f"{frame_idx:08d}.jpg")
-        C.save(f_save)
-
-        print(f_save)
-
-    """
-    for t in ITR:
-
-        # Easing motion, useful for regular SLERP
-        a = motion(t)
-
-        # Linear motion, better for spherical_bezier
-        #a = t        
-
-        frame_idx += 1
-
-        print(f_save)
-    """
+        scipy.misc.imsave(f_save, img)
 
 
-G, D, Gs = load_GAN_model()
 
-target_idx = 360
-# target_idx = 165
-target_idx = 662  # blue goth
-# target_idx = 17048
-# target_idx = 44751
+def load_z(n):
+    if os.path.exists(n):
+        f_npy = n
+    else:
+        f_npy = f"samples/latent_vectors/{n:06d}.npy"
 
+    assert(os.path.exists(f_npy))
+    z = np.load(f_npy)
 
-save_dest = f"motion/transport_walk/{target_idx:06d}"
-os.system(f"rm -rf {save_dest}")
-os.system(f"mkdir -p {save_dest}")
+    name = os.path.basename(f_npy).split('.')[0]
+    return z, name
+        
 
-f_npy = f"samples/latent_vectors/{target_idx:06d}.npy"
-z = np.load(f_npy)
+if __name__ == "__main__":
 
-epsilon = 0.40
-n_transitions = 20
+    np.random.seed(43)
+    
+    z0, name = load_z(360)
+    z0, name = load_z("saved/AliS.npy")
 
-# Vicinity sampling around the center
-Z = z + epsilon * np.random.standard_normal(size=(n_transitions - 1, 512))
+    G, D, Gs = load_GAN_model()
 
-# Add the starting point back on
-Z = np.vstack([Z, Z[0]])
+    save_dest = f"motion/transport_walk/{name}"
+    save_dest_ref = f"motion/transport_walk/{name}_reference"
 
-# Scale factor (see arXiv:1711.01970 Table 1)
-Z /= np.sqrt(1 + epsilon ** 2)
+    os.system(f"rm -rf {save_dest} {save_dest_ref}")
+    os.system(f"mkdir -p {save_dest} {save_dest_ref}")
 
-interpolate(Z, frames_per=30)
+    epsilon = 0.30
+    n_transitions = 20
+
+    # Vicinity sampling around the center
+    delta = epsilon * np.random.standard_normal(size=(n_transitions - 1, 512))
+
+    # Cherry-pick some bad samples away
+    for k in [12, 6, 0, 6, 6]:
+        delta[k] = np.random.standard_normal(size=(512,))
+    
+    Z = z0 + delta
+
+    # Build the reference samples
+    logger.debug(f"Building the reference samples in {save_dest_ref}")
+    for n, zx in tqdm(enumerate(Z)):
+        img, *_ = generate_single(Gs, D, z=zx, compute_discriminator=False)
+        f_save = os.path.join(save_dest_ref, f"{n:08d}.jpg")
+        scipy.misc.imsave(f_save, img)
+    
+    # Add the starting point back on for smooth loops 
+    Z = np.vstack([Z, Z[0]])
+
+    # Scale factor (see arXiv:1711.01970 Table 1)
+    Z /= np.sqrt(1 + epsilon ** 2)
+
+    interpolate(Z, frames_per=30)
