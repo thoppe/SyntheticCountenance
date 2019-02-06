4dc7630a27560415b32c0c3ce97507832d67c7d6
thoppe <travis.hoppe@gmail.com>
2019-02-06 17:28:29 -0500

    Cleaned noise

--- stylegan/P1_sample_noise.py
+++ stylegan/P1_sample_noise.py
@@ -9,22 +9,15 @@ import dnnlib.tflib as tflib
 n_samples = 10 ** 5
 random_seed = 42
 
+f_z = 'samples/latent_vectors/00000042.npy'
+z_base = np.load(f_z)
+
+###################################################################
+
 np.random.seed(random_seed)
 G, D, Gs = load_GAN_model()
 save_dest_imgs = "samples/images_noise"
-#save_dest_info = "samples/latent_vectors"
-
 os.system(f"mkdir -p {save_dest_imgs}")
-#os.system(f"mkdir -p {save_dest_info}")
-
-largest_known = glob.glob(os.path.join(save_dest_imgs, "*"))
-largest_idx = 0
-
-if len(largest_known):
-    largest_known = os.path.basename(sorted(largest_known)[-1])
-    largest_idx = int(largest_known.split(".")[0])
-
-#logger.info(f"Starting generation at {largest_idx}")
 
 noise_vars = [
     var for name, var in
@@ -32,20 +25,12 @@ noise_vars = [
 
 noise_pairs = list(zip(noise_vars, tflib.run(noise_vars)))
 
-
-for i in range(43):
-    z = np.random.randn(512)
-
-
 # Observation: layer 4 is the hair wave!
-n = 0
-for k in tqdm(range(len(noise_vars))):
-    print(f"Starting level {k}")
-    for _ in range(10):
 
-        f_save = os.path.join(save_dest_imgs, f"{n:08d}.jpg")
-        #if os.path.exists(f_save):
-        #    continue
+for k in tqdm(range(len(noise_vars))):
+    for n in tqdm(range(20)):
+
+        f_save = os.path.join(save_dest_imgs, f"level_{k:02d}_{n:08d}.jpg")
 
         noise_index = k
         nx = noise_vars[noise_index]
@@ -53,11 +38,7 @@ for k in tqdm(range(len(noise_vars))):
         small_noise = np.random.standard_normal(nx.shape)
         tflib.set_vars({nx:small_noise})
 
-        img, z, ds = generate_single(Gs, D, z=z, compute_discriminator=False)
+        img, _, _ = generate_single(Gs, D, z=z_base)
 
         P_img = Image.fromarray(img)
         P_img.save(f_save)
-
-        #f_save_info = os.path.join(save_dest_info, f"{n:08d}.npy")
-        #np.save(f_save_info, z)
-        n += 1
