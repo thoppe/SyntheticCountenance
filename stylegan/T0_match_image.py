64c6e604236cc39d589c2743cdc352677d6ee8ab
Travis Hoppe <travis.hoppe@gmail.com>
2019-02-10 16:11:29 -0500

    Multi T0 attempt

--- stylegan/T0_match_image.py
+++ stylegan/T0_match_image.py
@@ -8,154 +8,79 @@ Usage:
 
 Options:
   -h --help     Show this screen.
-  --learning_rate=<f>  ADAM learning rate [default: 0.0025]
+  --learning_rate=<f>  ADAM learning rate [default: 0.05]
   --restart
 """
+
+"""
+Training 00000042.jpg on 250 epochs, loss:
+1] Single z: 0.6198  (2.2k npy files)
+2] Multi z (10/18 layers): 0.3794 (2.2k npy file, but looks bad!?)
+3] Single z (with noise):
+4] Multi z (with noise):
+"""
+
 from docopt import docopt
 
-import dlib
+#import dlib
 import numpy as np
 from PIL import Image
 import os, json, glob
 import dnnlib.tflib as tflib
 
 import tensorflow as tf
-from src.GAN_model import load_GAN_model, logger, generate_single, RGB_to_GAN_output
+from src.GAN_model import load_GAN_model, logger, generate_single
+from src.GAN_model import RGB_to_GAN_output
 
 import cv2
 import sklearn.decomposition
-import PIL
-from PIL import Image
 
-np.random.seed(46)
+np.random.seed(48)
 n_image_upscale = 1
 
-
-model_dest = "model/dlib"
-shape68_pred = dlib.shape_predictor(
-    f"{model_dest}/shape_predictor_68_face_landmarks.dat"
-)
-detector = dlib.get_frontal_face_detector()
-
-
-def compute_keypoints(img, bbox):
-
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    shape68 = shape68_pred(gray, bbox)
-    keypoints = np.array(face_utils.shape_to_np(shape68))
-
-    return keypoints
-
-
-def compute_bbox(img, n_upsample=0):
-    faces = detector(img, n_upsample)
-    if len(faces) != 1:
-        logger.warning(f"Found {len(faces)} faces in image! Expected one")
-    return faces[0]
-
-
-def single_img2latent_inference(f_image):
-
-    clf = Image2Latent().load()
-    raw_img = clf.preprocess_file(f_image)
-    z = clf(raw_img)
-    clf.sess.close()
-    return z
-
-def compute_convex_hull_face(img, eyes_only=False):
-    """ Assume img is loaded from cv2.imread """
-
-    bbox = compute_bbox(img, n_image_upscale)
-    assert(len(bbox)==1)
-    
-    keypoints = KEY68(img, bbox[0])
-
-    if eyes_only:
-        keypoints = keypoints[36:48]
-
-    hull = cv2.convexHull(keypoints)
-    mask = np.zeros(img.shape, np.uint8)
-
-    cv2.drawContours(mask, [hull], 0, 1, -1)
-
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
-    mask = cv2.dilate(mask, kernel, iterations=1)
-    mask = mask.astype(bool)[:, :, 0]
-
-    return mask
-
-    
-    
-def process_image(f_reference, f_image):
-    # Align by the keypoints
-
-    img0 = cv2.imread(f_reference)
-    img1 = cv2.imread(f_image)
-
-    img1 = scale_face(img0, img1)
-    img1 = align_to_eyes(img0, img1)
-    img1 = center_eyes(img1)
-
-    # Center crop
-    img1 = center_crop(img1)
-
-    # Scale to the right size
-    height, width = img1.shape[:2]
-    if((height,width) != (1024,1014)):
-        img1 = cv2.resize(img1, (1024, 1024))
-
-    #bbox1 = compute_bbox(img1)[0]
-    #k1 = KEY68(img1, bbox1).astype(np.float32)
-    #for x,y in k1[36:48]:
-    #    cv2.circle(img1,(x,y), 3, (255,255,255), -1)
-
-    #for x,y in k_old[36:48]:
-    #    cv2.circle(img1,(x,y), 3, (255,0,0), -1)
-    
-    # Blur around the face
-    mask = compute_convex_hull_face(img1)
-    img1[~mask] = cv2.blur(img1, (10,10))[~mask]
-    img1[~mask] = cv2.blur(img1, (10,10))[~mask]
-    
-    # DEBUG here
-    '''
-    bbox0 = compute_bbox(img0)[0]
-    k0 = KEY68(img0, bbox0).astype(np.float32)
-    for x,y in k0[36:48]:
-        cv2.circle(img1,(x,y), 3, (0,255,0), -1)
-
-    cv2.imshow('img1', img1)
-    cv2.waitKey(0)
-    '''
-
-    return img1
-    
-
+#model_dest = "model/dlib"
+#shape68_pred = dlib.shape_predictor(
+#    f"{model_dest}/shape_predictor_68_face_landmarks.dat"
+#)
+#detector = dlib.get_frontal_face_detector()
 
 class GeneratorInverse:
     def __init__(self, generator, sess, learning_rate=0.01,
-                 use_mask=True, z_init=None):
+                 use_mask=True, use_multi=True, z_init=None):
 
         self.sess = sess
         self.target_image = None
         self.use_mask = use_mask
+        self.use_multi = use_multi
 
         latent_dim = 512
         image_dim = 1024
         batch_size = 1
-
-        if z_init is None:
-            # Start with random init for the latents
-            z_init = np.random.randn(latent_dim)[None, :]
-
-        self.z = tf.Variable(z_init, dtype=tf.float32)
         
-        #self.mask = tf.placeholder(dtype=tf.float32)
+        # Single matching
+        if not self.use_multi:
+            z_init = np.random.randn(latent_dim)[None, :]
+            self.z = tf.Variable(z_init, dtype=tf.float32)
+            G_out = generator.get_output_for(self.z, None, is_training=False)
+            
+        # Multi matching
+        elif self.use_multi:
 
-        # Labels are not needed for this project
-        label_dummy = tf.zeros([batch_size, 0])
+            n_opt = 10
+            z_init0 = np.random.standard_normal(size=[n_opt, 512])
+            z_init1 = np.random.standard_normal(size=[18-n_opt, 512])
+                       
+            self.z = tf.Variable(z_init0, dtype=tf.float32)
+            self.z2 = tf.Variable(z_init1, dtype=tf.float32, trainable=False)
 
-        G_out = generator.get_output_for(self.z, label_dummy, is_training=False)
+            # The first one fails badly
+            #z_val = tf.concat([self.z, self.z2], 0)
+            z_val = tf.concat([self.z2, self.z], 0)
+
+            G_out = Gs.components.synthesis.get_output_for(
+                [z_val],
+                is_validation=True, randomize_noise=False,
+            )
 
         # NCHW
         self.img_in = tf.placeholder(
@@ -172,46 +97,32 @@ class GeneratorInverse:
         # Sum over all pixels
         #if use_mask:
         #    L1_loss *= self.mask
-
         self.loss = tf.reduce_sum(L1_loss ** 2)
         self.loss /= 1024**2
         #self.loss /= tf.reduce_sum(self.mask)
 
-        #self.loss += tf.abs(1 - (tf.linalg.norm(self.z) / np.sqrt(latent_dim)))
-
-        #self.opt = tf.train.AdamOptimizer(learning_rate=learning_rate)
-        self.opt = tf.train.GradientDescentOptimizer(
-            learning_rate=learning_rate)
+        self.opt = tf.train.AdamOptimizer(learning_rate=learning_rate)
+        #self.opt = tf.train.GradientDescentOptimizer(
+        #    learning_rate=learning_rate)
 
         # Only train the latent variable (hold the generator fixed!)
-        self.train_op = self.opt.minimize(self.loss, var_list=[self.z])
+        minimize_vars = [self.z,]
+        
+        self.train_op = self.opt.minimize(
+            self.loss, var_list=minimize_vars)
 
     def initialize(self):
-        self.sess.run(tf.initializers.variables([self.z]))
+        if self.use_multi:
+            self.sess.run(tf.initializers.variables([self.z, self.z2]))
+        else:
+            self.sess.run(tf.initializers.variables([self.z]))
+        
         self.sess.run(tf.initializers.variables(self.opt.variables()))
 
     def set_target(self, f_image):
         """
         For now, can only load from a file.
         """
-
-        '''
-        if self.use_mask:
-            # Load a mask (all CV2 operations)
-            cv_img = cv2.imread(f_image)
-            mask = compute_convex_hull_face(cv_img)
-            self.target_mask = np.ones_like(mask)
-
-            # Set values off the mask to still be important
-            self.target_mask[~mask] = 0.25
-
-            # Set the values around the eyes to be twice as important
-            eye_mask = compute_convex_hull_face(cv_img, eyes_only=True)
-            self.target_mask[eye_mask] = 2.0
-        else:
-            # Debug line
-            self.target_mask = np.ones((1024, 1024))
-        '''
         
         # Load the target image
         img = Image.open(f_image)
@@ -221,8 +132,10 @@ class GeneratorInverse:
         """
         Renders the current latent vector into an image.
         """
-        z_current = self.sess.run(
-            self.z,
+        tf_latent = self.z
+        
+        current_latent = self.sess.run(
+            tf_latent,
             feed_dict={
                 self.img_in: self.target_image,
                 #self.mask: self.target_mask
@@ -231,7 +144,7 @@ class GeneratorInverse:
         
         fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
         img = Gs.run(
-            z_current,
+            current_latent,
             None,
             truncation_psi=0.7,
             use_noise=False,
@@ -239,17 +152,12 @@ class GeneratorInverse:
             output_transform=fmt
         )[0]
 
-        print(img.shape)
-        
-        #img = Gs.run(z_current, np.zeros([512, 0]))
-        #img = GAN_output_to_RGB(img)[0]
-
         if f_save is not None:
             P_img = Image.fromarray(img)
             P_img.save(f_save)
 
             f_npy = f_save.replace(".jpg", ".npy")
-            np.save(f_npy, z_current)
+            np.save(f_npy, current_latent)
 
         return img
 
@@ -261,9 +169,11 @@ class GeneratorInverse:
 
         if self.target_image is None:
             raise ValueError("must call .set_target(img) first!")
+                
+        tf_latent = self.z
 
-        outputs = [self.loss, self.z, self.train_op]
-        lx, z, _ = self.sess.run(
+        outputs = [self.loss, tf_latent, self.train_op]
+        lx, current_latent, _ = self.sess.run(
             outputs,
             feed_dict={
                 self.img_in: self.target_image,
@@ -271,11 +181,7 @@ class GeneratorInverse:
             },
         )
 
-
-        return lx, z
-
-
-
+        return lx, current_latent
 
 
 
@@ -306,6 +212,8 @@ if __name__ == "__main__":
     #z_init = np.random.randn(512)
 
     G, D, Gs = load_GAN_model()
+
+    
     sess = tf.get_default_session()
     
     GI = GeneratorInverse(Gs, sess, learning_rate=learning_rate, z_init=None)
@@ -322,4 +230,5 @@ if __name__ == "__main__":
 
         loss, z = GI.train()
         norm = np.linalg.norm(z) / np.sqrt(512)
+        print(z.shape)
         logger.debug(f"Epoch {i}, loss {loss:0.4f}, z-norm {norm:0.4f}")
